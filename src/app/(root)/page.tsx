8a99bfe52cd490289954cbfed229b9a9e6e7a6ff
Yoseph1994 <joshrde2002@example.com>
2024-08-15 12:58:35 +0300

    font & assets config

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main>
+      <h1 className="text-4xl">EventFest</h1>
       <Button variant="destructive">Josi</Button>
     </main>
   );
